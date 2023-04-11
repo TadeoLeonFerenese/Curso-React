a3b6f34d3696178d7b80740c705db5ac91a16093
Tadeo <teoleon2013@gmail.com>
2023-04-11 12:41:12 -0300

    Loadin bb

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,10 @@
+// export function App() {
+//   // document.createElement...
+//   return <h1>Hola Mundo!!!</h1>;
+// }
+
+export const HelloWorldApp = () => {
+  return <h1>HelloWorldApp</h1>;
+};
+
+//FirstApp <h1>Tadeo</h1>
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import reactDom from "react-dom/client";
+import { HelloWorldApp } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+reactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelloWorldApp />
+    <FirstApp />
   </React.StrictMode>
-)
+);
