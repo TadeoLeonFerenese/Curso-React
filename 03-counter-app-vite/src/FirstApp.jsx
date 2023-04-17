73854cb142112539bc48b2edbbb5c9ff2b9422ae
Tadeo <teoleon2013@gmail.com>
2023-04-17 13:08:21 -0300

    On Fire

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,17 @@
+//Los  functionals components como este si se puede se dejan por fuera de la funcion principal por que consumen espacio en memoria cuando re re renderiza la apk
+const getSaludo = () => {
+  return "Hola Mundo :) ";
+};
+
+// const newMessage = "Tadeo!!";
+// si quiero mandar un objeto como child  si o si tenes que usar "JSON.stringify()"
+
 export const FirstApp = () => {
-  return <h1>Tadeo</h1>;
+  return (
+    //esto es un fragmento, sirve para utilizar dos elementos sin tener la necesidad de poner un div
+    <>
+      <h1>{getSaludo()}</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  );
 };
