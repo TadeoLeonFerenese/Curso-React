159be30fac876beadf20407586ee5582104f1549
Tadeo <teoleon2013@gmail.com>
2023-04-19 13:28:30 -0300

    On Fire

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,17 +1,18 @@
 //Los  functionals components como este si se puede se dejan por fuera de la funcion principal por que consumen espacio en memoria cuando re re renderiza la apk
-const getSaludo = () => {
-  return "Hola Mundo :) ";
-};
+// const getSaludo = () => {
+//   return "Hola Mundo :) ";
+// };
 
 // const newMessage = "Tadeo!!";
 // si quiero mandar un objeto como child  si o si tenes que usar "JSON.stringify()"
 
-export const FirstApp = () => {
+export const FirstApp = ({ title, subTitle }) => {
   return (
     //esto es un fragmento, sirve para utilizar dos elementos sin tener la necesidad de poner un div
     <>
-      <h1>{getSaludo()}</h1>
-      <p>Soy un subtitulo</p>
+      <h1>{title}</h1>
+      {/* <h1>{getSaludo()}</h1> */}
+      <p>{subTitle + 1}</p>
     </>
   );
 };
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import reactDom from "react-dom/client";
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
 reactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
-    <FirstApp />
+    {/* <HelloWorldApp /> */}
+    <FirstApp title="Hola, Soy Goku" subTitle={123} />
   </React.StrictMode>
 );
