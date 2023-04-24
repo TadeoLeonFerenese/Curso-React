e68712b296c6c0dcdbdea695da44c1527b0ab500
Tadeo <teoleon2013@gmail.com>
2023-04-24 13:52:05 -0300

    On road

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 //Los  functionals components como este si se puede se dejan por fuera de la funcion principal por que consumen espacio en memoria cuando re re renderiza la apk
 // const getSaludo = () => {
 //   return "Hola Mundo :) ";
@@ -6,13 +7,27 @@
 // const newMessage = "Tadeo!!";
 // si quiero mandar un objeto como child  si o si tenes que usar "JSON.stringify()"
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subTitle, name }) => {
   return (
     //esto es un fragmento, sirve para utilizar dos elementos sin tener la necesidad de poner un div
     <>
       <h1>{title}</h1>
       {/* <h1>{getSaludo()}</h1> */}
-      <p>{subTitle + 1}</p>
+      <p>{subTitle}</p>
+      <p>{name}</p>
     </>
   );
+};
+
+// las proptypes sirven para especificar que tipo de props estas enviando, si es string, number, bolean
+// El "isRequired" sirve para que sea si o si  enviado o salta un warning d e error
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired,
+};
+
+FirstApp.defaultProps = {
+  title: " No hay titulo ",
+  subTitle: " No hay subTitle ",
+  name: "Tadeo Leon",
 };
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,10 +3,12 @@ import reactDom from "react-dom/client";
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 import "./styles.css";
+import { CounterApp } from "./CounterApp";
 
 reactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HelloWorldApp /> */}
-    <FirstApp title="Hola, Soy Goku" subTitle={123} />
+    <FirstApp title="Hola, soy Goku" subTitle={123123} />
+    <CounterApp value={3640} />
   </React.StrictMode>
 );
