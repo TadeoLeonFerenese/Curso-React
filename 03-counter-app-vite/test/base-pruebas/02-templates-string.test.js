3b9b02192cc4d58eba9087e3d245c72bc2380a18
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-05-07 13:40:51 -0300

    On Fire bb

---
+++ 03-counter-app-vite/test/base-pruebas/02-templates-string.test.js
@@ -0,0 +1,11 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+import { expect, jest, test } from "@jest/globals";
+
+describe("Prueba en 02-template-string", () => {
+  test("getSaludo debe retornar 'Hola Tadeo' ", () => {
+    const name = "Tadeo";
+    const message = getSaludo(name);
+
+    expect(message).toBe(`Hola${name}`);
+  });
+});
