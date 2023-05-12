2e3d7506a9a8f134ebd338442aa07db00800c99c
Tadeo <teoleon2013@gmail.com>
2023-05-12 13:50:37 -0300

    On Fire bb

--- 03-counter-app-vite/test/base-pruebas/06-deses-obj.test.js
+++ 03-counter-app-vite/test/base-pruebas/06-deses-obj.test.js
@@ -3,8 +3,8 @@ import { expect } from "@jest/globals";
 
 describe(" Prueba en 06-deses-obj ", () => {
   test("usContext debe retornar ", () => {
-    const key = "BetoaSabe";
-    const age = "31";
+    const key = "Tadeo";
+    const age = 31;
     const useContexto = usContext(key, age);
 
     expect(useContexto).toEqual({
